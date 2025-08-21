f404427fcda41b934fded7b9a421c2f25f19ab96
Pinal Gadiyal <gadiyalpinal@gmail.com>
2025-08-22 00:28:50 +0530

    Update CS045-4.c

--- CS045-4.c
+++ CS045-4.c
@@ -4,6 +4,7 @@ void main()
     int dollars, rupees;
     printf("Enter the number of dollars:  ");
     scanf("%d", & dollars);
-    ruppes = dollars * 48;
-    printf("%d $ is equal to %d Rs.", dollars, ruppes);
+    rupees = dollars * 48;
+    printf("%d $ is equal to %d Rs.", dollars, rupees);
+
 }
